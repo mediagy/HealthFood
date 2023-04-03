cef3058197b6c19f53fe4b975a61d9e94914567e
mediagy <267472544@qq.com>
2023-04-03 13:45:35 +0800

    个人中心3.4_ 实现登录

--- app/src/main/java/com/example/healthfood/fragment/PersonalCenter_frag.java
+++ app/src/main/java/com/example/healthfood/fragment/PersonalCenter_frag.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.healthfood.LoginActivity;
 import com.example.healthfood.R;
 
 public class PersonalCenter_frag extends Fragment {
@@ -20,6 +22,22 @@ public class PersonalCenter_frag extends Fragment {
 //        return super.onCreateView(inflater, container, savedInstanceState);
         //R.layout.frag_personalcenter是指res/layout文件夹中的frag_personalcenter.xml布局文件
         View view=inflater.inflate(R.layout.frag_personalcenter,null);
+        jumplogin(view);//跳转到LoginActivity中
         return view;
+    }
+
+    //跳转到LoginActivity中
+    public void jumplogin(View v) {
+        //找到布局文件frag_personalcenter.xml中的TextView“待君登录”，与变量tv_login关联
+        TextView tv_login = v.findViewById(R.id.tv_jumplogin);
+        tv_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //创建一个Intent类型变量intent（显式“意图”），准备从当前Activity跳转到LoginActivity（登录界面）
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                //开始新的Activity
+                startActivity(intent);
+            }
+        });
     }
 }
